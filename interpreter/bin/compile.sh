119adb673795962d44e1586835ce583058c921e2
Rares Cosma <raresc589@gmail.com>
2025-06-03 23:10:53 +0300

    rebuilt parser architecture from line-style (script) to token-style parsing (programming language)

--- interpreter/bin/compile.sh
+++ interpreter/bin/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-g++ -std=c++17 -Wall -Wextra ../src/lexer.cpp ../src/parser.cpp ../src/commons.cpp ../src/interpreter.cpp ../src/roscript.cpp -o ros
+g++ -std=c++17   -Wall -Wextra ../src/lexer.cpp ../src/parser.cpp ../src/commons.cpp ../src/interpreter.cpp ../src/roscript.cpp -o ros
 
 cp ros ../../roscript-ide
+echo "Finished compiling."  
